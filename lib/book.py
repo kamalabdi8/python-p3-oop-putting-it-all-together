d83d028d9187f10254b680e26ae0dc61c0f96eb7
kamalabdi8 <kamalabdi042@gmail.com>
2024-12-02 12:30:00 +0300

    first commit

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,65 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count, author="Unknown", genre="Unknown", price=0.0):
+        self.title = title
+        self.author = author
+        self.genre = genre
+        self.price = price
+        self.page_count = page_count
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 100:
+            self._title = value
+        else:
+            raise ValueError("Title must be a string between 1 and 100 characters.")
+
+    @property
+    def author(self):
+        return self._author
+
+    @author.setter
+    def author(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 50:
+            self._author = value
+        else:
+            raise ValueError("Author name must be a string between 1 and 50 characters.")
+
+    @property
+    def genre(self):
+        return self._genre
+
+    @genre.setter
+    def genre(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 30:
+            self._genre = value
+        else:
+            raise ValueError("Genre must be a string between 1 and 30 characters.")
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if isinstance(value, (int, float)) and value >= 0:
+            self._price = value
+        else:
+            raise ValueError("Price must be a non-negative number.")
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int) and value > 0:
+            self._page_count = value
+        else:
+            print("page_count must be an integer")
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")        
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,56 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size, color="Black", price=100):
+        self.brand = brand
+        self.size = size
+        self.color = color
+        self.price = price
+
+    @property
+    def brand(self):
+        return self._brand
+
+    @brand.setter
+    def brand(self, value):
+        if isinstance(value, str):
+            self._brand = value
+        else:
+            raise ValueError("Brand must be a string.")
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if  not isinstance(value, int):
+            print("size must be an integer") 
+            self._size = value
+        else:
+             
+             self._size = value
+
+    @property
+    def color(self):
+        return self._color
+
+    @color.setter
+    def color(self, value):
+        if isinstance(value, str):
+            self._color = value
+        else:
+            raise ValueError("Color must be a string.")
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if isinstance(value, (int, float)) and value > 0:
+            self._price = value
+        else:
+            raise ValueError("Price must be a positive number.")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"   
